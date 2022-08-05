603b586d70db2b48fde19fb2c417c2e651ff4bba
DESKTOP-ESDPF32\Dell <vuvietquang90@gmail.com>
2022-08-05 22:23:34 +0700

    Add back-end for pizza_app

---
+++ stores/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .serializers import PizzeriaListSerializer, PizzeriaDetailSerializer
+from .models import Pizzeria
+# Create your views here.
+
+class PizzeriaListAPIView(generics.ListAPIView):
+    queryset = Pizzeria.objects.all()
+    serializer_class = PizzeriaListSerializer
+
+class PizzeriaRetrieveAPIView(generics.RetrieveAPIView):
+    lookup_field = "id"
+    queryset = Pizzeria.objects.all()
+    # queryset = Pizzeria.objects.get(id=lookup_field)
+    serializer_class = PizzeriaDetailSerializer
+
+class PizzeriaCreateAPIView(generics.CreateAPIView):
+    queryset = Pizzeria.objects.all()
+    serializer_class = PizzeriaDetailSerializer
+
+class PizzeriaRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
+    lookup_field = "id"
+    queryset = Pizzeria.objects.all()
+    serializer_class = PizzeriaDetailSerializer
+
+class PizzeriaDestroyAPIView(generics.DestroyAPIView):
+    lookup_field = "id"
+    queryset = Pizzeria.objects.all()
